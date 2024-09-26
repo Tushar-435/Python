19fb5f2bda680d1dab44878b055053b3514bcad9
Tushar Singh <122785295+Tushar-435@users.noreply.github.com>
2024-09-26 19:36:47 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,37 @@
+
+#if else statment for given a vote 
+age=int(input("Enter the age of the person"))
+
+if age>=18:
+   print("Tushar is aligible for vote")
+else:
+   print("Tushar is not aligible for vote") 
+       
+
+
+price=int(input("Enter the number :"))
+'''
+if num%2==0 :
+    print("even number")
+else:
+    print("odd number5")'''
+
+
+'''if num%7==0 :
+    print("the numbr is the divisible of 7")
+else :
+    print("the number is not a divisible of 7")'''
+
+"""if num%5==0 :
+    print("the numbr is the multiple  of 5")
+else :
+    print("bye")"""
+
+'''if price<100:
+    print("no charge")
+elif price<=200:
+    print("Rs5 per unit")
+elif price>200:
+    print("10 per unit")'''
+
+
