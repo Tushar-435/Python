eddd078270f145f07fbf3ccacc87b58703dc0132
Tushar Singh <122785295+Tushar-435@users.noreply.github.com>
2024-09-26 19:33:42 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,14 @@
+#print hello noida
+print("Hello Noida")
+
+a=10
+b=20
+c=a+b
+print(c)
+
+# TAkE input from user 
+a=int(input("print the first number"))
+b=int(input("print the Second number"))
+c=int(input("print the Third number"))
+d=a+b+c
+print("print the sum of value:",d)
