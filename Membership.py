bc43d68f3edec19525f98e538fd58669071e2322
Tushar Singh <122785295+Tushar-435@users.noreply.github.com>
2024-09-30 09:45:33 +0530

    Add files via upload

---
+++ Membership.py
@@ -0,0 +1,15 @@
+x=int(input("Enter the value"))
+y="singh"
+
+myLit=[10,20,35,54,15]
+myString='tusharsingh'
+
+if (x not in myLit):
+	print("True")
+else:
+	print("false")
+
+if (y in myString):
+	print("true")
+else:
+	print("false")
